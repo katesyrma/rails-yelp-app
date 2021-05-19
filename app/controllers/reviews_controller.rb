97cd53a055acc7a76f81b5cc8f801e9154987051
Kate Syrmakesi <kate.syrmakesi@gmail.com>
2021-05-19 16:49:11 +0200

    slight refactoring

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,7 +8,6 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
-    @review.save
 
     if @review.save
       redirect_to restaurant_path(@restaurant)
